df6ce09edc56d21df27f91fc4562243114f4de93
Lavish Saluja <lavish@Lavishs-MacBook-Pro.local>
2024-07-06 13:49:36 +0530

    refactor: use 2 spaces for indentation octokit.js

--- src/octokit.js
+++ src/octokit.js
@@ -1,37 +1,37 @@
 const getRepositoryTree = async (octokit, owner, repo, branch, recursive) => {
-    if (recursive === undefined) {
-        recursive = true;
-    }
+  if (recursive === undefined) {
+    recursive = true;
+  }
 
-    const res = await octokit.git.getTree({
-        owner: owner,
-        repo: repo,
-        tree_sha: branch,
-        recursive: true
-    });
+  const res = await octokit.git.getTree({
+    owner: owner,
+    repo: repo,
+    tree_sha: branch,
+    recursive: true
+  });
 
-    const files = res.data.tree;
-    const fileNames = files.map(f => f.path);
+  const files = res.data.tree;
+  const fileNames = files.map(f => f.path);
 
-    return fileNames;
+  return fileNames;
 }
 
 const getRepository = async (octokit, owner, repo) => {
-    const res = await octokit.repos.get({
-        owner: owner,
-        repo: repo
-    });
+  const res = await octokit.repos.get({
+    owner: owner,
+    repo: repo
+  });
 
-    return res.data;
+  return res.data;
 };
 
 const getDefaultBranch = async (octokit, owner, repo) => {
-    const repository = await getRepository(octokit, owner, repo);
+  const repository = await getRepository(octokit, owner, repo);
 
-    return repository.default_branch;
+  return repository.default_branch;
 };
 
 module.exports = {
-    getRepositoryTree,
-    getDefaultBranch
+  getRepositoryTree,
+  getDefaultBranch
 }
